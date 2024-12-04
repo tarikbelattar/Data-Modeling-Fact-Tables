af523743f819a638e36d465b8009efa96d10951a
Tarik <tarikbelattar@gmail.com>
2024-12-04 11:08:39 -0500

    first commit Query3 finalized

--- DDL_user_devices_cumulated.sql
+++ DDL_user_devices_cumulated.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user_devices_cumulated(
-    user_id BIGINT,
+    user_id NUMERIC,
     device_type text,
     device_activity_datelist DATE[],
     date DATE,
-    primary key (user_id, date)
+    primary key (user_id, date, device_type)
 )
--- DROP TABLE user_devices_cumulated
+--DROP TABLE user_devices_cumulated
--- Query3_cumulative_query.sql
+++ Query3_cumulative_query.sql
@@ -2,7 +2,7 @@
 WITH yesterday AS(
     SELECT *
     FROM user_devices_cumulated
-    WHERE date = DATE('2022-12-31')
+    WHERE date = DATE('2023-01-04')
 ),
   Today AS(
     SELECT
@@ -11,19 +11,20 @@ WITH yesterday AS(
         array_agg(DISTINCT (DATE(e.event_time))) AS device_activity_list
     FROM events e
     INNER JOIN devices d on e.device_id = d.device_id
-    WHERE DATE(e.event_time) = DATE('2023-01-01')
+    WHERE DATE(e.event_time) = DATE('2023-01-05')
     AND e.user_id IS NOT NULL
     GROUP BY e.user_id, d.device_type
     )
 INSERT INTO user_devices_cumulated
-SELECT
+SELECT DISTINCT
     COALESCE(y.user_id, t.user_id) AS user_id,
     COALESCE(y.device_type, t.device_type) AS device_type,
     CASE
         WHEN y.device_activity_datelist IS NULL THEN t.device_activity_list
         WHEN y.device_activity_datelist IS NOT NULL THEN y.device_activity_datelist || t.device_activity_list
         ELSE y.device_activity_datelist
-    END AS device_activity_list
+    END AS device_activity_list,
+    DATE('2023-01-05') AS date
 FROM yesterday y
 FULL OUTER JOIN today t
 ON y.user_id = t.user_id
